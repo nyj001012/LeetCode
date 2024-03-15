4304e494843523f56e6c33f0bd957d5bd967c0ab
yena <50291995+nyj001012@users.noreply.github.com>
2024-03-15 15:37:27 +0900

    Time: 110 ms (71.55%), Space: 48.5 MB (54.51%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int[] Shuffle(int[] nums, int n) {
+        int[] shuffle = new int[2 * n];
+        
+        for (int i = 0; i < n; i++) {
+            shuffle[i * 2] = nums[i];
+            shuffle[i * 2 + 1] = nums[n + i];
+        }
+        return shuffle;
+    }
+}
